b56b1235800771503fea940c23a88f941d63388b
patribezek <patribezek@gmail.com>
2016-09-25 23:52:31 -0300

    Add files via upload

---
+++ js/custom_js.js
@@ -0,0 +1,29 @@
+window.setTimeout("closeDiv();", 7000);
+
+function closeDiv(){
+document.getElementById("pageLoader").style.display=" none";
+}
+
+
+
+// Hello there!
+
+// This project is a mess, so i know (now) i have not the experience you require 
+// to be part of MediaMonks. 
+
+// But i want a chance. 
+
+// I worked in this simple project 5 hours the sunday, a few hours more today, monday. The reason
+// to keep trying is because i really want to be part of your company. I simply love the 
+// projects i could see in your webpage, and that's exactly what i want for my grow,  
+// to learn to do that awesome things. 
+
+// Thank you in advance. 
+
+// Sincerely, 
+
+// Patricia Bezek. 
+
+
+
+
